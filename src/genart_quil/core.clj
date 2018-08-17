2e51d15233056be346cbf500e5563a02d95e59b3
t-sin <shinichi.tanaka45@gmail.com>
2018-08-17 16:46:00 +0900

    Sine circles

--- src/genart_quil/core.clj
+++ src/genart_quil/core.clj
@@ -4,33 +4,17 @@
 
 (defn setup []
   ; Set frame rate to 30 frames per second.
-  (q/frame-rate 30)
+  (q/frame-rate 3)
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  {:color 0
-   :angle 0})
-
-(defn update-state [state]
-  ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
-
-(defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
-  (q/background 240)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 100 100))))
+  (q/background 150 40 40)
+  (q/fill 0 0 0 0)
+  (q/stroke 160 30 255 100)
+  (doseq [x (range 0 500)]
+    (let [th (/ x 10)
+          th2 (/ x 3.15)
+          r (* 200 (q/sin th2))]
+      (q/ellipse x (+ 250 (* 100 (q/sin th))) r r))))
 
 (defn -main [& args]
   (q/defsketch genart-quil
@@ -39,8 +23,8 @@
     ; setup function called only once, during sketch initialization.
     :setup setup
     ; update-state is called on each iteration before draw-state.
-    :update update-state
-    :draw draw-state
+;    :update update-state
+;    :draw draw-state
     :features [:keep-on-top]
     ; This sketch uses functional-mode middleware.
     ; Check quil wiki for more info about middlewares and particularly
